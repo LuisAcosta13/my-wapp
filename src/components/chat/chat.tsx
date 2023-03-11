d0a225949a2a3ec1dd87262161c5c17c6bcd7f60
Luis Acosta <acosta1395@hotmail.com>
2023-03-11 17:44:59 -0300

    se agregó burbuja de chat e input funcional

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -5,6 +5,8 @@ import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
 import TagFacesIcon from '@mui/icons-material/TagFaces';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
+import { useState } from 'react';
 
 const Chat: React.FunctionComponent = () => {
 
@@ -14,6 +16,22 @@ const Chat: React.FunctionComponent = () => {
     let tagFacesIcon: JSX.Element = <TagFacesIcon/>
     let attachFileIcon: JSX.Element = <AttachFileIcon/>
     let keyboardVoiceIcon: JSX.Element = <KeyboardVoiceIcon/>
+    let doneAllOutlinedIcon: JSX.Element = <DoneAllOutlinedIcon/>
+
+    const [input, setInput] = useState({message: '', time: ''})
+    const [chat, setChat] = useState<any[]>([])
+
+    const handleInputChange = (e: any) => {
+        setInput({message: e.target.value, time: new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })})
+    }
+
+    const handleSubmit = () => {
+        setChat([...chat, input] as any)
+        setInput({
+            message: '', 
+            time: ''
+        })
+    }
 
     return(
         <div className={style.container}>
@@ -29,7 +47,21 @@ const Chat: React.FunctionComponent = () => {
                 </div>
             </div>
             <div className={style.second_container}>
-                
+                <span className={style.scroll}>
+                <div className={style.second_subcontainer}>
+                    {chat.map(c => 
+                    <span className={style.chat_container}>
+                        <span className={style.chat_bubble}>
+                            {c.message}
+                        </span>
+                        <span className={style.hour}>
+                            {c.time} 
+                            <span className={style.check_icon}>
+                                {doneAllOutlinedIcon}
+                            </span>
+                        </span>
+                    </span>)}
+                </div></span>
             </div>
             <div className={style.third_container}>
                 <div className={style.icons_chat}>
@@ -49,6 +81,10 @@ const Chat: React.FunctionComponent = () => {
                     data-toggle="tooltip" 
                     data-placement="bottom-right" 
                     title="Escribe un mensaje aquí"
+                    value={input.message}
+                    autoComplete="off"
+                    onChange={e => handleInputChange(e)}
+                    onKeyDown={(e) =>{if(e.key === 'Enter'){handleSubmit()}}}
                     />
                         
                 </div>
